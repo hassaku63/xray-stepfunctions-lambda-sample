b12e25e32beebb8258d93f5f8213fed813fc07f7
hassaku63 <hassaku63@gmail.com>
2022-10-13 19:41:06 +0900

    fix

--- handler.py
+++ handler.py
@@ -5,20 +5,21 @@ from aws_xray_sdk.core import xray_recorder
 # from aws_xray_sdk.core import patch_all
 
 
-xray_recorder.configure(service='My app')
+xray_recorder.configure(service='my-app')
 # patch_all()
 
 
 def handler1(event, context):
     # サブセグメント (1)
-    subsegment = xray_recorder.begin_subsegment('my_subsegment')
+    subsegment_1 = xray_recorder.begin_subsegment('my_subsegment_1')
     sleep(random.random())  # 何かしらの（計測対象にしたい）処理を実行する
-    xray_recorder.end_segment()
+    subsegment_1.put_annotation('mykey', 'foo')
+    xray_recorder.end_subsegment()
 
     # サブセグメント (2)
-    subsegment = xray_recorder.begin_subsegment('my_subsegment')
+    subsegment_2 = xray_recorder.begin_subsegment('my_subsegment_2')
     sleep(random.random())  # 何かしらの（計測対象にしたい）処理を実行する
-    xray_recorder.end_segment()
+    xray_recorder.end_subsegment()
 
     return event
 
